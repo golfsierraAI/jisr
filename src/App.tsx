9fd6c06c928a1bfced0e1cfe84fdf2b904f311aa
GolfSierra <gsharma774962@gmail.com>
2024-09-11 16:45:23 +0530

    renaming variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <Folder files={files} />
+      <Folder root={files} />
     </div>
   );
 }
--- src/components/Folder/index.tsx
+++ src/components/Folder/index.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { useState } from "react";
 import PopUp from "../PopUp/index.tsx";
 
-function Folder({ files }) {
+function Folder({ root }) {
   const [expand, setExpand] = useState(false);
   const [showPopUp, setShowPopUp] = useState(false);
 
@@ -14,16 +14,16 @@ function Folder({ files }) {
     }
   };
 
-  if (files.type === "folder") {
+  if (root.type === "folder") {
     return (
       <div>
         <div onClick={() => setExpand(!expand)} className="folder">
-          <span>📁 {files.name}</span>
+          <span>📁 {root.name}</span>
         </div>
 
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
-          {files.data.map((exp, index) => {
-            return <Folder key={index} files={exp} />;
+          {root.data.map((exp, index) => {
+            return <Folder key={index} root={exp} />;
           })}
         </div>
       </div>
@@ -32,9 +32,9 @@ function Folder({ files }) {
     return (
       <div className="popup_wrapper">
         <span className="file" onContextMenu={handlePopUp}>
-          📄 {files.name}
+          📄 {root.name}
         </span>
-        <PopUp showPopUp={showPopUp} fileName={files.name} />
+        <PopUp showPopUp={showPopUp} fileName={root.name} />
       </div>
     );
   }
