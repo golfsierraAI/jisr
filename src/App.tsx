4f085a8ce311bb94cb6a2aba4f3a8ef3370362f5
GolfSierra <gsharma774962@gmail.com>
2024-09-11 16:35:17 +0530

    remove root

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Folder from "./components/Folder/index.tsx";
-import { files } from "./data";
+import { files } from "./data.js";
 import React from "react";
 
 export default function App() {
